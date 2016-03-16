5f07fd2d3591ac7bf4e8eb844ef197659b58d7fe
EthanHeilman <ethan.r.heilman@gmail.com>
2016-03-16 09:54:30 -0700

    Fix de-serialization bug where AddrMan is corrupted after exception
    * CAddrDB modified so that when de-serialization code throws an exception Addrman is reset to a clean state
    * CAddrDB modified to make unit tests possible
    * Regression test created to ensure bug is fixed
    * StartNode modifed to clear adrman if CAddrDB::Read returns an error code.

--- src/net.cpp
+++ src/net.cpp
@@ -1737,8 +1737,10 @@ void StartNode(boost::thread_group& threadGroup, CScheduler& scheduler)
         CAddrDB adb;
         if (adb.Read(addrman))
             LogPrintf("Loaded %i addresses from peers.dat  %dms\n", addrman.size(), GetTimeMillis() - nStart);
-        else
+        else {
+            addrman.Clear(); // Addrman can be in an inconsistent state after failure, reset it
             LogPrintf("Invalid or missing peers.dat; recreating\n");
+        }
     }
 
     uiInterface.InitMessage(_("Loading banlist..."));
@@ -2055,6 +2057,11 @@ bool CAddrDB::Read(CAddrMan& addr)
     if (hashIn != hashTmp)
         return error("%s : Checksum mismatch, data corrupted", __func__);
 
+    return Read(addr, ssPeers);
+}
+
+bool CAddrDB::Read(CAddrMan& addr, CDataStream& ssPeers)
+{
     unsigned char pchMsgTmp[4];
     try {
         // de-serialize file header (network specific magic number) and ..
@@ -2067,6 +2074,8 @@ bool CAddrDB::Read(CAddrMan& addr)
         // de-serialize address data into one CAddrMan object
         ssPeers >> addr;
     } catch (const std::exception& e) {
+        // de-serialization has failed, ensure addrman is left in a clean state
+        addr.Clear();
         return error("%s : Deserialize or I/O error - %s", __func__, e.what());
     }
 
