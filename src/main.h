43af66be8c67daedeac4efa56a5cd5ca413ed620
presstab <presstab1337@gmail.com>
2017-11-22 15:24:35 -0700

    Add message popup when db corrupt. Make ReindexAccumulators() use ref list.

--- src/main.h
+++ src/main.h
@@ -205,7 +205,7 @@ bool LoadExternalBlockFile(FILE* fileIn, CDiskBlockPos* dbp = NULL);
 /** Initialize a new block tree database + block data on disk */
 bool InitBlockIndex();
 /** Load the block tree and coins database from disk */
-bool LoadBlockIndex();
+bool LoadBlockIndex(std::string& strError);
 /** Unload database information */
 void UnloadBlockIndex();
 /** See whether the protocol update is enforced for connected nodes */
@@ -374,7 +374,7 @@ bool ValidOutPoint(const COutPoint out, int nHeight);
 void RecalculateZPIVSpent();
 void RecalculateZPIVMinted();
 bool RecalculatePIVSupply(int nHeightStart);
-bool ReindexAccumulators(list<uint256> listMissingCheckpoints, string& strError);
+bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError);
 
 
 /**
