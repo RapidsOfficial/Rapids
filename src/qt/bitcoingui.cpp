99a8246f18fa77ea800fbe57be5e376342dac6c1
Mrs-X <Mrs-X@users.noreply.github.com>
2017-12-24 02:11:19 +0100

    [UI] Spelling error fixed anonimization -> anonymization

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1207,7 +1207,7 @@ void BitcoinGUI::setEncryptionStatus(int status)
     case WalletModel::UnlockedForAnonymizationOnly:
         labelEncryptionIcon->show();
         labelEncryptionIcon->setIcon(QIcon(":/icons/lock_open").pixmap(STATUSBAR_ICONSIZE, STATUSBAR_ICONSIZE));
-        labelEncryptionIcon->setToolTip(tr("Wallet is <b>encrypted</b> and currently <b>unlocked</b> for anonimization and staking only"));
+        labelEncryptionIcon->setToolTip(tr("Wallet is <b>encrypted</b> and currently <b>unlocked</b> for anonymization and staking only"));
         encryptWalletAction->setChecked(true);
         changePassphraseAction->setEnabled(true);
         unlockWalletAction->setVisible(true);
