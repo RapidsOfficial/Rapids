1736e9a2bbc36a83a940bccc4d88de78412080c9
presstab <presstab1337@gmail.com>
2018-03-23 18:42:20 -0600

    Fix zPIV mint databasing.

--- src/wallet.h
+++ src/wallet.h
@@ -212,6 +212,7 @@ public:
     bool GetZerocoinKey(const CBigNum& bnSerial, CKey& key);
     bool CreateZPIVOutPut(libzerocoin::CoinDenomination denomination, libzerocoin::PrivateCoin& coin, CTxOut& outMint);
     bool GetMint(const uint256& hashSerial, CZerocoinMint& mint);
+    bool DatabaseMint(libzerocoin::PrivateCoin* coin);
 
     /** Zerocin entry changed.
     * @note called with lock cs_wallet held.
