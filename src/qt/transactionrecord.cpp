73abc887beeb2fc70688434f9e4cc53e70af4a1e
warrows <warrows@gamer666.fr>
2018-03-27 16:43:54 +0200

    [Qt] Fix zPIV transactions details

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -74,12 +74,10 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet* 
                 sub.type = TransactionRecord::StakeZPIV;
                 sub.address = mapValue["zerocoinmint"];
                 sub.credit = nNet;
-
                 for (const CTxOut& out : wtx.vout) {
                     if (out.IsZerocoinMint())
-                        sub.debit += out.nValue;
+                        sub.credit += out.nValue;
                 }
-
                 sub.debit -= wtx.vin[0].nSequence * COIN;
             } else {
                 isminetype mine = wallet->IsMine(wtx.vout[1]);
@@ -103,7 +101,6 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet* 
                 TransactionRecord sub(hash, nTime);
                 sub.type = TransactionRecord::ZerocoinSpend_Change_zPiv;
                 sub.address = mapValue["zerocoinmint"];
-                sub.debit = -txout.nValue;
                 if (!fFeeAssigned) {
                     sub.debit -= (wtx.GetZerocoinSpent() - wtx.GetValueOut());
                     fFeeAssigned = true;
@@ -122,8 +119,12 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet* 
             isminetype mine = wallet->IsMine(txout);
             if (mine) {
                 TransactionRecord sub(hash, nTime);
-                sub.type = (fZSpendFromMe ? TransactionRecord::ZerocoinSpend_FromMe : TransactionRecord::RecvFromZerocoinSpend);
-                sub.debit = txout.nValue;
+                if (fZSpendFromMe) {
+                    sub.type = TransactionRecord::ZerocoinSpend_FromMe;
+                } else {
+                    sub.type = TransactionRecord::RecvFromZerocoinSpend;
+                    sub.credit = txout.nValue;
+                }
                 sub.address = mapValue["recvzerocoinspend"];
                 if (strAddress != "")
                     sub.address = strAddress;
@@ -269,6 +270,7 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet* 
                 } else if (txout.IsZerocoinMint()){
                     sub.type = TransactionRecord::ZerocoinMint;
                     sub.address = mapValue["zerocoinmint"];
+                    sub.credit += txout.nValue;
                 } else {
                     // Sent to IP, or other non-address transaction like OP_EVAL
                     sub.type = TransactionRecord::SendToOther;
