fafc0845e80a75c9dea819f94ebc31ae67ca2563
Mrs-X <Mrs-X@users.noreply.github.com>
2018-04-24 14:58:57 +0200

    [Budget] Handling of too few votes for finalized budget fixed

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -701,6 +701,7 @@ TrxValidationStatus CBudgetManager::IsTransactionValid(const CTransaction& txNew
 
     std::string strProposals = "";
     int nCountThreshold = nHighestCount - mnodeman.CountEnabled(ActiveProtocol()) / 10;
+    bool fThreshold = false;
     it = mapFinalizedBudgets.begin();
     while (it != mapFinalizedBudgets.end()) {
         CFinalizedBudget* pfinalizedBudget = &((*it).second);
@@ -711,6 +712,7 @@ TrxValidationStatus CBudgetManager::IsTransactionValid(const CTransaction& txNew
                  nBlockHeight, pfinalizedBudget->GetVoteCount(), nCountThreshold);
 
         if (pfinalizedBudget->GetVoteCount() > nCountThreshold) {
+            fThreshold = true;
             LogPrint("mnbudget","CBudgetManager::IsTransactionValid - GetVoteCount() > nCountThreshold passed\n");
             if (nBlockHeight >= pfinalizedBudget->GetBlockStart() && nBlockHeight <= pfinalizedBudget->GetBlockEnd()) {
                 LogPrint("mnbudget","CBudgetManager::IsTransactionValid - GetBlockStart() passed\n");
@@ -732,7 +734,12 @@ TrxValidationStatus CBudgetManager::IsTransactionValid(const CTransaction& txNew
         ++it;
     }
 
-    //we looked through all of the known budgets
+    // If not enough masternodes autovoted for any of the finalized budgets pay a masternode instead
+    if(!fThreshold) {
+        transactionStatus = TrxValidationStatus::VoteThreshold;
+    }
+    
+    // We looked through all of the known budgets
     return transactionStatus;
 }
 
