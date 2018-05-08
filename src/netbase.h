0ec1fd5577ca5f75aa7681b282283c2d1ab3c023
gpdionisio <gianpiero.dionisio@protonmail.ch>
2018-05-08 03:18:11 +0200

    make use of getnameinfo() optional (default: true)

--- src/netbase.h
+++ src/netbase.h
@@ -82,7 +82,7 @@ public:
     bool IsMulticast() const;
     enum Network GetNetwork() const;
     std::string ToString() const;
-    std::string ToStringIP() const;
+    std::string ToStringIP(bool fUseGetnameinfo = true) const;
     unsigned int GetByte(int n) const;
     uint64_t GetHash() const;
     bool GetInAddr(struct in_addr* pipv4Addr) const;
@@ -167,9 +167,9 @@ public:
     friend bool operator!=(const CService& a, const CService& b);
     friend bool operator<(const CService& a, const CService& b);
     std::vector<unsigned char> GetKey() const;
-    std::string ToString() const;
+    std::string ToString(bool fUseGetnameinfo = true) const;
     std::string ToStringPort() const;
-    std::string ToStringIPPort() const;
+    std::string ToStringIPPort(bool fUseGetnameinfo = true) const;
 
     CService(const struct in6_addr& ipv6Addr, unsigned short port);
     CService(const struct sockaddr_in6& addr);
