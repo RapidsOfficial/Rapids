8a6d425672886dd82021d1c9f0dff61fe50441f8
presstab <presstab1337@gmail.com>
2018-05-09 15:53:09 -0600

    Add getaccumulatorvalues RPC.
    Return the accumulator values associated with a particular block height.

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -298,6 +298,7 @@ static const CRPCCommand vRPCCommands[] =
 
         /* Block chain and UTXO */
         {"blockchain", "findserial", &findserial, true, false, false},
+        {"blockchain", "getaccumulatorvalues", &getaccumulatorvalues, true, false, false},
         {"blockchain", "getblockchaininfo", &getblockchaininfo, true, false, false},
         {"blockchain", "getbestblockhash", &getbestblockhash, true, false, false},
         {"blockchain", "getblockcount", &getblockcount, true, false, false},
