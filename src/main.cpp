16531a5a83f9406ac954fb41a34a55fdd304f444
warrows <warrows@gamer666.fr>
2018-05-23 11:20:59 +0200

    [Core] Missing seesaw value for block 325000

--- src/main.cpp
+++ src/main.cpp
@@ -1870,7 +1870,7 @@ CAmount GetSeeSaw(const CAmount& blockValue, int nMasternodeCount, int nHeight)
     CAmount ret = 0;
     if (mNodeCoins == 0) {
         ret = 0;
-    } else if (nHeight < 325000) {
+    } else if (nHeight <= 325000) {
         if (mNodeCoins <= (nMoneySupply * .05) && mNodeCoins > 0) {
             ret = blockValue * .85;
         } else if (mNodeCoins <= (nMoneySupply * .1) && mNodeCoins > (nMoneySupply * .05)) {
