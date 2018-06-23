571b9fd6d47ebeccdcb8135d2af40f8b60d03e52
Fuzzbawls <fuzzbawls@gmail.com>
2018-06-23 03:32:53 -0700

    ensure override is set for GetSerialHash()

--- src/stakeinput.h
+++ src/stakeinput.h
@@ -24,7 +24,7 @@ public:
     virtual bool GetModifier(uint64_t& nStakeModifier) = 0;
     virtual bool IsZPIV() = 0;
     virtual CDataStream GetUniqueness() = 0;
-    virtual uint256 GetSerialHash() const = 0;
+    virtual uint256 GetSerialHash() = 0;
 };
 
 
@@ -59,7 +59,7 @@ public:
     bool CreateTxOuts(CWallet* pwallet, vector<CTxOut>& vout, CAmount nTotal) override;
     bool MarkSpent(CWallet* pwallet, const uint256& txid);
     bool IsZPIV() override { return true; }
-    uint256 GetSerialHash() const { return hashSerial; }
+    uint256 GetSerialHash() override { return hashSerial; }
     int GetChecksumHeightFromMint();
     int GetChecksumHeightFromSpend();
     uint32_t GetChecksum();
@@ -86,7 +86,7 @@ public:
     bool CreateTxIn(CWallet* pwallet, CTxIn& txIn, uint256 hashTxOut = 0) override;
     bool CreateTxOuts(CWallet* pwallet, vector<CTxOut>& vout, CAmount nTotal) override;
     bool IsZPIV() override { return false; }
-    uint256 GetSerialHash() const { return uint256(0); }
+    uint256 GetSerialHash() override { return uint256(0); }
 };
 
 
