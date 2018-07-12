f668d67846368997cae2beac17237e24b30d9480
Fuzzbawls <fuzzbawls@gmail.com>
2018-07-11 23:26:07 -0700

    Remove security level from UI

--- src/qt/privacydialog.h
+++ src/qt/privacydialog.h
@@ -49,7 +49,7 @@ public:
     void setZPivControlLabels(int64_t nAmount, int nQuantity);
 
 public slots:
-    void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance, 
+    void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
                     const CAmount& zerocoinBalance, const CAmount& unconfirmedZerocoinBalance, const CAmount& immatureZerocoinBalance,
                     const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
 protected:
@@ -70,8 +70,7 @@ private:
     CAmount currentWatchOnlyBalance;
     CAmount currentWatchUnconfBalance;
     CAmount currentWatchImmatureBalance;
-    
-    int nSecurityLevel = 0;
+
     bool fMinimizeChange = false;
     bool fDenomsMinimized;
 
