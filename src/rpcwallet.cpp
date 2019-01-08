6c2df854d98e634f95d22b7b6117e42962072c42
Fuzzbawls <fuzzbawls@gmail.com>
2019-01-07 23:03:53 -0800

    Ensure wallet is unlocked for createautomintaddress
    Also changed the return to a simple string (instead of a JSON object) to be consistent with other RPC commands that return just a single address.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -3641,16 +3641,18 @@ UniValue searchdzpiv(const UniValue& params, bool fHelp)
 UniValue createautomintaddress(const UniValue& params, bool fHelp)
 {
     if (fHelp || params.size() != 0)
-        throw runtime_error(
+        throw std::runtime_error(
                 "createautomintaddress\n"
-                        "Overview: generates new auto mint address\n"
-                        "\nResult\n"
-                        "\"address\"             160hash of public key\n" +
+                "\nGenerates new auto mint address\n" +
+                HelpRequiringPassphrase() + "\n"
+
+                "\nResult\n"
+                "\"address\"     (string) PIVX address for auto minting\n" +
                 HelpExampleCli("createautomintaddress", "") +
                 HelpExampleRpc("createautomintaddress", ""));
+
+    EnsureWalletIsUnlocked();
     LOCK(pwalletMain->cs_wallet);
     CBitcoinAddress address = pwalletMain->GenerateNewAutoMintKey();
-    UniValue ret(UniValue::VOBJ);
-    ret.push_back(Pair("address", address.ToString()));
-    return ret;
+    return address.ToString();
 }
