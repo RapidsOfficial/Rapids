2159a5860151df5c4415f245b551112697961be9
Fuzzbawls <fuzzbawls@gmail.com>
2019-01-18 01:33:32 -0800

    Add newly created automint addresses to the set
    Ensures that a wallet restart isn't required after generating a new automint address by adding it to the set.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -120,6 +120,7 @@ CBitcoinAddress CWallet::GenerateNewAutoMintKey()
     btcAddress.Set(keyID);
     CWalletDB(strWalletFile).WriteAutoConvertKey(btcAddress);
     SetAddressBook(keyID, "automint-address", "receive");
+    setAutoConvertAddresses.emplace(btcAddress);
     return btcAddress;
 }
 
