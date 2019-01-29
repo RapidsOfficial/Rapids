158bd2b215599eaca2d1292fa1d5034dbb03a032
furszy <5377650+furszy@users.noreply.github.com>
2019-01-29 01:00:12 +0100

    regtest PoS generate valid blocks

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2123,7 +2123,7 @@ bool CWallet::SelectStakeCoins(std::list<std::unique_ptr<CStakeInput> >& listInp
             }
 
             //check for min age
-            if (GetAdjustedTime() - nTxTime < nStakeMinAge)
+            if ((GetAdjustedTime() - nTxTime < nStakeMinAge ) && Params().NetworkID() != CBaseChainParams::REGTEST)
                 continue;
 
             //check that it is matured
