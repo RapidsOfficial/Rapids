5de083f1d68a0bca5d62db368ff4057fc8004fb8
cevap <dev@i2pmail.org>
2019-02-07 19:02:57 +0100

    Fix qt for sparc64

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -110,6 +110,7 @@ $(package)_config_opts_riscv64_linux = -platform linux-g++ -xplatform pivx-linux
 $(package)_config_opts_s390x_linux += -platform linux-g++ -xplatform linux-g++-64
 $(package)_config_opts_powerpc_linux += -platform linux-g++ -xplatform linux-g++-32
 $(package)_config_opts_powerpc64le_linux += -platform linux-g++ -xplatform linux-g++-64
+$(package)_config_opts_sparc64_linux += -platform linux-g++ -xplatform linux-g++-64
 $(package)_config_opts_mingw32  = -no-opengl -xplatform win32-g++ -device-option CROSS_COMPILE="$(host)-"
 $(package)_build_env  = QT_RCC_TEST=1
 $(package)_build_env += QT_RCC_SOURCE_DATE_OVERRIDE=1
