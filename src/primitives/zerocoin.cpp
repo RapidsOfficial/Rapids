29c1791815bb5675e5ef53985d8541157d3cdce1
furszy <5377650+furszy@users.noreply.github.com>
2019-03-18 15:58:26 +0100

    inflation methods moved to cpp

--- src/primitives/zerocoin.cpp
+++ src/primitives/zerocoin.cpp
@@ -76,3 +76,40 @@ int CZerocoinSpendReceipt::GetNeededSpends()
 {
     return nNeededSpends;
 }
+
+
+int GetWrapppedSerialInflation(libzerocoin::CoinDenomination denom){
+    if(Params().NetworkID() == CBaseChainParams::MAIN) {
+        switch (denom) {
+            case libzerocoin::CoinDenomination::ZQ_ONE:
+                return 7;
+            case libzerocoin::CoinDenomination::ZQ_FIVE:
+                return 6;
+            case libzerocoin::CoinDenomination::ZQ_TEN:
+                return 36;
+            case libzerocoin::CoinDenomination::ZQ_FIFTY:
+                return 22;
+            case libzerocoin::CoinDenomination::ZQ_ONE_HUNDRED:
+                return 244;
+            case libzerocoin::CoinDenomination::ZQ_FIVE_HUNDRED:
+                return 22;
+            case libzerocoin::CoinDenomination::ZQ_ONE_THOUSAND:
+                return 42;
+            case libzerocoin::CoinDenomination::ZQ_FIVE_THOUSAND:
+                return 98;
+            default:
+                throw std::runtime_error("GetWrapSerialInflation :: Invalid denom");
+        }
+    }else{
+        // Testnet/Regtest is ok.
+        return 0;
+    }
+}
+
+int64_t GetWrapppedSerialInflationAmount(){
+    int64_t amount = 0;
+    for (auto& denom : libzerocoin::zerocoinDenomList){
+        amount += libzerocoin::ZerocoinDenominationToAmount(denom);
+    }
+    return amount;
+}
