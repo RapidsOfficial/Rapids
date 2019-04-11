b5af23513f1d3d8818767d1ba835c6511697dc72
blockchaindev727 <vangyangpao@gmail.com>
2019-04-12 02:35:58 +0700

    last release

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ More information at [https://rapidsnetwork.io]
 | Max Coin Supply             | 35,000,000,000 RPD            |
 | Premine                     | 20,000,000,000 RPD            |
 | Masternode Collateral       | 10M RPD                       |
-| Reward Rate per block       | MN 66.(6)%. POS 33.(3)%.      |    
+| Reward Rate per block       | MN 60%. POS 30%. Burn 10%     |    
 | Halving Cycle               | 4years (2102400 blocks)       |
 
 ### Reward Distribution
@@ -20,9 +20,9 @@ More information at [https://rapidsnetwork.io]
 |  **Block Height**  | **Reward per block** 
 |--------------------|-----------------------|
 | 1                  |  20,000,000,000 RPD   | 
-| 2-2102401          |  3210.6168 RPD        |
-| 2102402-4204801    |  1605.3084 RPD        | 
-| 4204802-6307201    |  802.6542 RPD         |
-| 6307202-8409601    |  401.3271 RPD         | 
-| 8409602-10512001   |  200.66355 RPD        | 
-| 10512002-Infinite  |  100.331775 RPD       | 
+| 2-2102401          |  3567.352 RPD         |
+| 2102402-4204801    |  1783.676 RPD         | 
+| 4204802-6307201    |  891.838 RPD          |
+| 6307202-8409601    |  445.919 RPD          | 
+| 8409602-10512001   |  222.959 RPD          | 
+| 10512002-Infinite  |  111.480 RPD          | 
---
+++ src/keystore.h
@@ -0,0 +1,95 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2017 The PIVX developers
+// Copyright (c) 2017-2018 The Rapids developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_KEYSTORE_H
+#define BITCOIN_KEYSTORE_H
+
+#include "key.h"
+#include "pubkey.h"
+#include "sync.h"
+
+#include <boost/signals2/signal.hpp>
+
+class CScript;
+class CScriptID;
+
+/** A virtual base class for key stores */
+class CKeyStore
+{
+protected:
+    mutable CCriticalSection cs_KeyStore;
+
+public:
+    virtual ~CKeyStore() {}
+
+    //! Add a key to the store.
+    virtual bool AddKeyPubKey(const CKey& key, const CPubKey& pubkey) = 0;
+    virtual bool AddKey(const CKey& key);
+
+    //! Check whether a key corresponding to a given address is present in the store.
+    virtual bool HaveKey(const CKeyID& address) const = 0;
+    virtual bool GetKey(const CKeyID& address, CKey& keyOut) const = 0;
+    virtual void GetKeys(std::set<CKeyID>& setAddress) const = 0;
+    virtual bool GetPubKey(const CKeyID& address, CPubKey& vchPubKeyOut) const;
+
+    //! Support for BIP 0013 : see https://github.com/bitcoin/bips/blob/master/bip-0013.mediawiki
+    virtual bool AddCScript(const CScript& redeemScript) = 0;
+    virtual bool HaveCScript(const CScriptID& hash) const = 0;
+    virtual bool GetCScript(const CScriptID& hash, CScript& redeemScriptOut) const = 0;
+
+    //! Support for Watch-only addresses
+    virtual bool AddWatchOnly(const CScript& dest) = 0;
+    virtual bool RemoveWatchOnly(const CScript& dest) = 0;
+    virtual bool HaveWatchOnly(const CScript& dest) const = 0;
+    virtual bool HaveWatchOnly() const = 0;
+
+    //! Support for MultiSig addresses
+    virtual bool AddMultiSig(const CScript& dest) = 0;
+    virtual bool RemoveMultiSig(const CScript& dest) = 0;
+    virtual bool HaveMultiSig(const CScript& dest) const = 0;
+    virtual bool HaveMultiSig() const = 0;
+};
+
+typedef std::map<CKeyID, CKey> KeyMap;
+typedef std::map<CScriptID, CScript> ScriptMap;
+typedef std::set<CScript> WatchOnlySet;
+typedef std::set<CScript> MultiSigScriptSet;
+
+/** Basic key store, that keeps keys in an address->secret map */
+class CBasicKeyStore : public CKeyStore
+{
+protected:
+    KeyMap mapKeys;
+    ScriptMap mapScripts;
+    WatchOnlySet setWatchOnly;
+    MultiSigScriptSet setMultiSig;
+
+public:
+    bool AddKeyPubKey(const CKey& key, const CPubKey& pubkey);
+    bool HaveKey(const CKeyID& address) const;
+    void GetKeys(std::set<CKeyID>& setAddress) const;
+    bool GetKey(const CKeyID& address, CKey& keyOut) const;
+
+    virtual bool AddCScript(const CScript& redeemScript);
+    virtual bool HaveCScript(const CScriptID& hash) const;
+    virtual bool GetCScript(const CScriptID& hash, CScript& redeemScriptOut) const;
+
+    virtual bool AddWatchOnly(const CScript& dest);
+    virtual bool RemoveWatchOnly(const CScript& dest);
+    virtual bool HaveWatchOnly(const CScript& dest) const;
+    virtual bool HaveWatchOnly() const;
+
+    virtual bool AddMultiSig(const CScript& dest);
+    virtual bool RemoveMultiSig(const CScript& dest);
+    virtual bool HaveMultiSig(const CScript& dest) const;
+    virtual bool HaveMultiSig() const;
+};
+
+typedef std::vector<unsigned char, secure_allocator<unsigned char> > CKeyingMaterial;
+typedef std::map<CKeyID, std::pair<CPubKey, std::vector<unsigned char> > > CryptedKeyMap;
+
+#endif // BITCOIN_KEYSTORE_H
