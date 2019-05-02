3555f430d2c250492d7362a77379a23c9a3b0116
random-zebra <random.zebra@protonmail.com>
2019-05-02 11:18:29 +0200

    [Wallet] Disable automint

--- src/main.cpp
+++ src/main.cpp
@@ -3580,11 +3580,15 @@ void static UpdateTip(CBlockIndex* pindexNew)
 {
     chainActive.SetTip(pindexNew);
 
+    /* Zerocoin minting is disabled
+     *
 #ifdef ENABLE_WALLET
     // If turned on AutoZeromint will automatically convert PIV to zPIV
     if (pwalletMain && pwalletMain->isZeromintEnabled())
         pwalletMain->AutoZeromint();
 #endif // ENABLE_WALLET
+    *
+    */
 
     // New best block
     nTimeBestReceived = GetTime();
