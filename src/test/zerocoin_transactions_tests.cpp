0393227b6588c783f01edf1fc8bd8f128ab90ea2
random-zebra <random.zebra@protonmail.com>
2019-05-19 23:14:04 +0200

    [Tests] add test reorg for public spend
    [Unit Test] zc public spend test up-to-date with master.

--- src/test/zerocoin_transactions_tests.cpp
+++ src/test/zerocoin_transactions_tests.cpp
@@ -12,13 +12,14 @@
 #include "wallet/walletdb.h"
 #include "txdb.h"
 #include "zpiv/zpivmodule.h"
+#include "test/test_pivx.h"
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 
 using namespace libzerocoin;
 
 
-BOOST_AUTO_TEST_SUITE(zerocoin_transactions_tests)
+BOOST_FIXTURE_TEST_SUITE(zerocoin_transactions_tests, TestingSetup)
 
 static CWallet cWallet("unlocked.dat");
 
