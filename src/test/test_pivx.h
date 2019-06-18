ef8df80d783b4d4871bc74c658693ac1b670a84e
Fuzzbawls <fuzzbawls@gmail.com>
2019-06-17 23:46:50 -0700

    [Trivial] Update header copyright years
    This updates the PIVX copyright year in file headers to the year that
    they were last updated.

--- src/test/test_pivx.h
+++ src/test/test_pivx.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2015 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef PIVX_TEST_TEST_PIVX_H
 #define PIVX_TEST_TEST_PIVX_H
 
--- src/zpiv/witness.cpp
+++ src/zpiv/witness.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <chainparams.h>
 #include <tinyformat.h>
 #include "witness.h"
