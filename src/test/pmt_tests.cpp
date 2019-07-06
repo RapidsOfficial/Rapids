f53edec73901a91a6c4f595b248e4058e8be5468
warrows <warrows@gamer666.fr>
2019-07-06 11:28:16 +0200

    [Rand/test] scripted-diff: Use new naming style for insecure_rand* functions
    -BEGIN VERIFY SCRIPT-
    sed -i 's/\<insecure_randbits(/InsecureRandBits(/g' src/test/*.cpp
    src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_randbool(/InsecureRandBool(/g' src/test/*.cpp
    src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_randrange(/InsecureRandRange(/g' src/test/*.cpp
    src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_randbytes(/InsecureRandBytes(/g' src/test/*.cpp
    src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_rand256(/InsecureRand256(/g' src/test/*.cpp
    src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<insecure_rand(/InsecureRand32(/g' src/test/*.cpp
    src/test/*.h src/wallet/test/*.cpp
    sed -i 's/\<seed_insecure_rand(/SeedInsecureRand(/g' src/test/*.cpp
    src/test/*.h src/wallet/test/*.cpp
    -END VERIFY SCRIPT-

--- src/test/pmt_tests.cpp
+++ src/test/pmt_tests.cpp
@@ -20,8 +20,8 @@ class CPartialMerkleTreeTester : public CPartialMerkleTree
 public:
     // flip one bit in one of the hashes - this should break the authentication
     void Damage() {
-        unsigned int n = insecure_randrange(vHash.size());
-        int bit = insecure_randbits(8);
+        unsigned int n = InsecureRandRange(vHash.size());
+        int bit = InsecureRandBits(8);
         uint256 &hash = vHash[n];
         hash ^= ((uint256)1 << bit);
     }
@@ -61,7 +61,7 @@ BOOST_AUTO_TEST_CASE(pmt_test1)
             std::vector<bool> vMatch(nTx, false);
             std::vector<uint256> vMatchTxid1;
             for (unsigned int j=0; j<nTx; j++) {
-                bool fInclude = insecure_randbits(att / 2) == 0;
+                bool fInclude = InsecureRandBits(att / 2) == 0;
                 vMatch[j] = fInclude;
                 if (fInclude)
                     vMatchTxid1.push_back(vTxid[j]);
--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -243,7 +243,7 @@ BOOST_AUTO_TEST_CASE(util_seed_insecure_rand)
     int i;
     int count=0;
 
-    seed_insecure_rand(true);
+    SeedInsecureRand(true);
 
     for (int mod=2;mod<11;mod++)
     {
@@ -259,7 +259,7 @@ BOOST_AUTO_TEST_CASE(util_seed_insecure_rand)
         {
             uint32_t rval;
             do{
-                rval=insecure_rand()&mask;
+                rval=InsecureRand32()&mask;
             }while(rval>=(uint32_t)mod);
             count += rval==0;
         }
