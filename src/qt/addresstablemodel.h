c477e9b6d975150810d341eadc10e1a81dc3731c
furszy <matiasfurszyfer@protonmail.com>
2019-07-21 12:46:06 -0300

    [Wallet][Model] GetKeyCreationTime moved from walletModel into the wallet object + addressTableModel new field for the address creation time.

--- src/qt/addresstablemodel.h
+++ src/qt/addresstablemodel.h
@@ -27,7 +27,8 @@ public:
 
     enum ColumnIndex {
         Label = 0,  /**< User specified label */
-        Address = 1 /**< Bitcoin address */
+        Address = 1, /**< Bitcoin address */
+        Date = 2 /**< Address creation date */
     };
 
     enum RoleIndex {
