6a33fffd0fbd74f673afe9673a77d1d0875f4118
furszy <matiasfurszyfer@protonmail.com>
2019-08-14 15:01:11 -0300

    [Mempool] // As zero fee transactions are not going to be accepted in the near future (4.0) and the code will be fully refactored soon.           // This is just a quick inline towards that goal, the mempool by default will not accept them. Blocking           // any subsequent network relay.
    [SQUASH] Permitting free txes only in regtest.

--- src/main.cpp
+++ src/main.cpp
@@ -1576,6 +1576,16 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState& state, const CTransa
                 hash.ToString(),
                 nFees, ::minRelayTxFee.GetFee(nSize) * 10000);
 
+        // As zero fee transactions are not going to be accepted in the near future (4.0) and the code will be fully refactored soon.
+        // This is just a quick inline towards that goal, the mempool by default will not accept them. Blocking
+        // any subsequent network relay.
+        if ((Params().NetworkID() != CBaseChainParams::REGTEST) &&
+            nFees == 0 && !tx.HasZerocoinSpendInputs()) {
+            return error("%s: zero fees not accepted %s, %d > %d",
+                         __func__,
+                         hash.ToString(),
+                         nFees, ::minRelayTxFee.GetFee(nSize) * 10000);
+        }
 
         bool fCLTVHasMajority = CBlockIndex::IsSuperMajority(5, chainActive.Tip(), Params().EnforceBlockUpgradeMajority());
 
