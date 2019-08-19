41ccf6b86b48949fc18dae6728359f87073770b1
furszy <matiasfurszyfer@protonmail.com>
2019-08-19 15:28:21 -0300

    [PoS][Wallet] CreateCoinStake, if the block is from the future then start on a future time.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2371,6 +2371,14 @@ bool CWallet::CreateCoinStake(
     CScript scriptPubKeyKernel;
     bool fKernelFound = false;
     int nAttempts = 0;
+
+    // Block time.
+    nTxNewTime = GetAdjustedTime();
+    // If the block time is in the future, then starts there.
+    if (pindexPrev->nTime > nTxNewTime) {
+        nTxNewTime = pindexPrev->nTime;
+    }
+
     for (std::unique_ptr<CStakeInput>& stakeInput : listInputs) {
         nCredit = 0;
         // Make sure the wallet is unlocked and shutdown hasn't been requested
@@ -2378,7 +2386,6 @@ bool CWallet::CreateCoinStake(
             return false;
 
         uint256 hashProofOfStake = 0;
-        nTxNewTime = GetAdjustedTime();
         nAttempts++;
         //iterates each utxo inside of CheckStakeKernelHash()
         if (Stake(pindexPrev, stakeInput.get(), nBits, nTxNewTime, hashProofOfStake)) {
