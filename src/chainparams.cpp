bf1651452cef40f72f6edc4a71515c15a24c6264
random-zebra <random.zebra@protonmail.com>
2019-08-25 23:59:37 +0200

    [Consensus] New modifier enforcement height changed for mainnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -178,7 +178,7 @@ public:
         nBlockDoubleAccumulated = 1050010;
         nEnforceNewSporkKey = 1525158000; //!> Sporks signed after (GMT): Tuesday, May 1, 2018 7:00:00 AM GMT must use the new spork key
         nRejectOldSporkKey = 1527811200; //!> Fully reject old spork key after (GMT): Friday, June 1, 2018 12:00:00 AM
-        nBlockStakeModifierlV2 = 1964000;
+        nBlockStakeModifierlV2 = 1967000;
         // Public coin spend enforcement
         nPublicZCSpends = 1880000;
 
