e0e9498c7a735da77d416224246407d7485c11aa
furszy <matiasfurszyfer@protonmail.com>
2019-09-29 14:09:23 -0300

    [Logging][Startup]  * Stop loading block indexes on wallet startup if shutdown was requested.  * Wallet loading, wallet rescan and block index load time logged in a more understandable way.

--- src/util.cpp
+++ src/util.cpp
@@ -114,7 +114,6 @@ bool fDebug = false;
 bool fPrintToConsole = false;
 bool fPrintToDebugLog = true;
 bool fDaemon = false;
-bool fServer = false;
 std::string strMiscWarning;
 bool fLogTimestamps = false;
 bool fLogIPs = false;
