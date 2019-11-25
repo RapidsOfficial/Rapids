65d81bac559e56698fed76ef31dc8405780cc739
random-zebra <random.zebra@protonmail.com>
2019-11-25 09:19:55 +0100

    [Net] Protocol update enforcement for 70918

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70917;
+static const int PROTOCOL_VERSION = 70918;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,8 +20,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70916;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70917;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70917;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70918;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
 static const int MIN_PEER_MNANNOUNCE = 70913;
