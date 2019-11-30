9253412554562fa0195c3074121f5dd36951d5e9
NoobieDev12 <57232689+NoobieDev12@users.noreply.github.com>
2019-11-30 17:55:11 +0100

    [GUI][Trivial] Rewording of Error message while trying to send with the transaction with wallet unlocked for staking only
    Not sure if this is good or it needs to be:
    
    > Error: The wallet is unlocked for anonymization and staking only.**\n**Unlock the wallet to send the transaction.

--- src/qt/pivx/guitransactionsutils.cpp
+++ src/qt/pivx/guitransactionsutils.cpp
@@ -50,7 +50,7 @@ namespace GuiTransactionsUtils {
                 if (!fPrepare)
                     fAskForUnlock = true;
                 else
-                    msgParams.first = parent->translate("Error: The wallet was unlocked only to anonymize coins.");
+                    msgParams.first = parent->translate("Error: The wallet is unlocked for staking only. Fully unlock the wallet to send the transaction.");
                 break;
 
             case WalletModel::InsaneFee:
