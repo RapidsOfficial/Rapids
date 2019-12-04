84c8a7fcb82fc1bf306630cd7aae68f755a3c862
Fuzzbawls <fuzzbawls@gmail.com>
2019-12-04 14:19:32 -0800

    [Qt] properly copy IPv6 externalip MN info
    IPv6 addresses use `:` rather than `.` characters in their notation,
    so needed to change the way `externalip` was being copied here.

--- src/qt/pivx/masternodeswidget.cpp
+++ src/qt/pivx/masternodeswidget.cpp
@@ -240,7 +240,7 @@ void MasterNodesWidget::onInfoMNClicked(){
                 ))) {
             // export data
             QString exportedMN = "masternode=1\n"
-                                 "externalip=" + address.split(":")[0] + "\n" +
+                                 "externalip=" + address.left(address.lastIndexOf(":")) + "\n" +
                                  "masternodeaddr=" + address + + "\n" +
                                  "masternodeprivkey=" + index.sibling(index.row(), MNModel::PRIV_KEY).data(Qt::DisplayRole).toString() + "\n";
             GUIUtil::setClipboard(exportedMN);
