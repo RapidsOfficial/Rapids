0187f0dfae850c8f46326ffdb59cadf863c378d1
random-zebra <random.zebra@protonmail.com>
2020-01-03 16:51:17 +0100

    [Cleanup][Tests] Remove crazy useful unittest created by "Tom"

--- src/test/zerocoin_chain_tests.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2018-2019 The PIVX developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-//
-// Created by tom on 2/11/18.
-//
-
