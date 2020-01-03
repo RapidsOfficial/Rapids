29a934c6a4955af046d140c7485c9d6d7e390cab
random-zebra <random.zebra@protonmail.com>
2020-01-03 16:50:17 +0100

    [Cleanup][Wallet] Remove zpiv spend cache from zpiv tracker

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -48,7 +48,6 @@ extern bool bSpendZeroConfChange;
 extern bool bdisableSystemnotifications;
 extern bool fSendFreeTransactions;
 extern bool fPayAtLeastCustomFee;
-extern bool fGlobalUnlockSpendCache; // Bool used for letting the precomputing thread know that zpivspends need to use the cs_spendcache
 
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;
