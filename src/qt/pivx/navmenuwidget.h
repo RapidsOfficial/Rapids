7c524b9d5e05e9d2ae46e1efabe0444c6c624e13
furszy <matiasfurszyfer@protonmail.com>
2020-01-14 08:34:21 -0300

    [GUI] Force NavMenu to show dashboard button at startup.

--- src/qt/pivx/navmenuwidget.h
+++ src/qt/pivx/navmenuwidget.h
@@ -23,6 +23,7 @@ public:
     ~NavMenuWidget();
 
     void loadWalletModel() override;
+    virtual void showEvent(QShowEvent *event) override;
 
 public slots:
     void selectSettings();
@@ -44,6 +45,8 @@ private:
 
     void connectActions();
     void onNavSelected(QWidget* active, bool startup = false);
+
+    bool init = false;
 };
 
 #endif // NAVMENUWIDGET_H
