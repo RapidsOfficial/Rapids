f578a68d1599013ab658ec36ccfd7766c3b6f7e8
Fuzzbawls <fuzzbawls@gmail.com>
2020-01-23 23:23:34 -0800

    Move CValidationState to consensus subdir

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 #include "checkpoints.h"
 #include "checkqueue.h"
 #include "consensus/merkle.h"
+#include "consensus/validation.h"
 #include "init.h"
 #include "kernel.h"
 #include "masternode-budget.h"
