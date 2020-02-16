d368d97b070eecab2b21560902d49699f12cdf7e
furszy <matiasfurszyfer@protonmail.com>
2020-02-15 22:54:36 -0300

    Sync.h: Upstream updates
    1) Refactor:
    * Call sync.h primitives "locks" and "mutexes" instead of "blocks" and "waitable
    critical sections" to match current coding conventions and c++11 standard
    names.
    
    2) Make LOCK, LOCK2, TRY_LOCK work with Mutex
    
    3) WAIT_LOCK added.

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,7 @@ CBlockIndex* pindexBestHeader = NULL;
 int64_t nTimeBestReceived = 0;
 
 // Best block section
-CWaitableCriticalSection g_best_block_mutex;
+Mutex g_best_block_mutex;
 std::condition_variable g_best_block_cv;
 uint256 g_best_block;
 
