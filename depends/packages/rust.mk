e78d9f13a0dbe702bdbb47693f99a7b660077dec
furszy <matiasfurszyfer@protonmail.com>
2020-02-21 14:31:21 -0300

    [Build] rust, platform specific files and hashes (linux added)

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,10 @@
 package=rust
 $(package)_version=1.36.0
 $(package)_download_path=https://static.rust-lang.org/dist
-$(package)_file_name=rust-$($(package)_version)-x86_64-apple-darwin.tar.gz
-$(package)_sha256_hash=91f151ec7e24f5b0645948d439fc25172ec4012f0584dd16c3fb1acb709aa325
+$(package)_file_name_linux=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
+$(package)_sha256_hash_linux=15e592ec52f14a0586dcebc87a957e472c4544e07359314f6354e2b8bd284c55
+$(package)_file_name_darwin=rust-$($(package)_version)-x86_64-apple-darwin.tar.gz
+$(package)_sha256_hash_darwin=91f151ec7e24f5b0645948d439fc25172ec4012f0584dd16c3fb1acb709aa325
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
