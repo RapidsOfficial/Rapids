668cd932478605bb76e678b80a9d19e9949fb123
random-zebra <random.zebra@protonmail.com>
2020-03-03 21:16:37 +0100

    [Tests] Set nStakeMinDepth to 2
    so stakeinputs must at least be on chain

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -404,7 +404,7 @@ public:
         consensus.nPoolMaxTransactions = 2;
         consensus.nProposalEstablishmentTime = 60 * 5;  // at least 5 min old to make it into a budget
         consensus.nStakeMinAge = 0;
-        consensus.nStakeMinDepth = 0;
+        consensus.nStakeMinDepth = 2;
         consensus.nTargetTimespan = 40 * 60;
         consensus.nTargetTimespanV2 = 30 * 60;
         consensus.nTargetSpacing = 1 * 60;
