e8c4d723beafe17b9b71f229957e81c4f70ae612
furszy <matiasfurszyfer@protonmail.com>
2020-03-03 11:54:06 -0300

    [Depends] Configure librustzcash for cross-compiling.
    [Depends] Update librustzcash lib.

--- depends/packages/librustzcash.mk
+++ depends/packages/librustzcash.mk
@@ -3,10 +3,21 @@ $(package)_version=0.1
 $(package)_download_path=https://github.com/zcash/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=a6554609ac0cbcc99ad33513a8203bec4ec1c64fa25b4be515377ccf2e0afcd7
-$(package)_git_commit=2a86c912f6ec32ae62c4403c214b2a93a5fcb61e
+$(package)_sha256_hash=c5442a57d8961aab12fd395a5004edbb96b973511fab3949a087faa2a865a002
+$(package)_git_commit=ef676eff5084d394e6c6eaf2b9d9817effe662a7
 $(package)_dependencies=rust $(rust_crates)
 $(package)_patches=cargo.config
+
+ifeq ($(host_os),mingw32)
+$(package)_library_file=target/x86_64-pc-windows-gnu/release/rustzcash.lib
+else
+$(package)_library_file=target/release/librustzcash.a
+endif
+
+define $(package)_set_vars
+$(package)_build_opts=--frozen --release
+$(package)_build_opts_mingw32=--target=x86_64-pc-windows-gnu
+endef
 
 define $(package)_preprocess_cmds
   mkdir .cargo && \
@@ -14,12 +25,12 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_build_cmds
-  cargo build --frozen --release
+  cargo build $($(package)_build_opts)
 endef
 
 define $(package)_stage_cmds
   mkdir $($(package)_staging_dir)$(host_prefix)/lib/ && \
   mkdir $($(package)_staging_dir)$(host_prefix)/include/ && \
-  cp target/release/librustzcash.a $($(package)_staging_dir)$(host_prefix)/lib/ && \
+  cp $($(package)_library_file) $($(package)_staging_dir)$(host_prefix)/lib/ && \
   cp include/librustzcash.h $($(package)_staging_dir)$(host_prefix)/include/
 endef
