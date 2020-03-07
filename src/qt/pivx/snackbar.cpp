e6953b29604717048a9ffadae8f03f4bac0d49d2
random-zebra <random.zebra@protonmail.com>
2020-03-08 00:00:08 +0100

    [GUI] Customize the timeout of the SnackBar based on its message length

--- src/qt/pivx/snackbar.cpp
+++ src/qt/pivx/snackbar.cpp
@@ -11,7 +11,8 @@
 SnackBar::SnackBar(PIVXGUI* _window, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SnackBar),
-    window(_window)
+    window(_window),
+    timeout(MIN_TIMEOUT)
 {
     ui->setupUi(this);
 
@@ -34,25 +35,38 @@ void SnackBar::windowResizeEvent(QResizeEvent* event) {
     this->move(QPoint(0, window->height() - this->height() ));
 }
 
-void SnackBar::showEvent(QShowEvent *event){
-    QTimer::singleShot(3000, this, &SnackBar::hideAnim);
+void SnackBar::showEvent(QShowEvent *event)
+{
+    QTimer::singleShot(timeout, this, &SnackBar::hideAnim);
 }
 
-void SnackBar::hideAnim(){
+void SnackBar::hideAnim()
+{
     if (window) closeDialog(this, window);
     QTimer::singleShot(310, this, &SnackBar::hide);
 }
 
-
-
-void SnackBar::sizeTo(QWidget* widget){
-
-}
-
-void SnackBar::setText(QString text){
+void SnackBar::setText(const QString& text)
+{
     ui->label->setText(text);
+    setTimeoutForText(text);
 }
 
-SnackBar::~SnackBar(){
+void SnackBar::setTimeoutForText(const QString& text)
+{
+    timeout = std::max(MIN_TIMEOUT, std::min(MAX_TIMEOUT, GetTimeout(text)));
+}
+
+int SnackBar::GetTimeout(const QString& message)
+{
+    // 50 milliseconds per char
+    return (50 * message.length());
+}
+
+SnackBar::~SnackBar()
+{
     delete ui;
 }
+
+const int SnackBar::MIN_TIMEOUT;
+const int SnackBar::MAX_TIMEOUT;
