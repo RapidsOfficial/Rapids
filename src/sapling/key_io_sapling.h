a446ec294b6b26035053d0d87e67321a518c9623
furszy <matiasfurszyfer@protonmail.com>
2020-03-21 14:00:23 -0300

    [Sapling] key_io basic methods back ported.
    * EncodePaymentAddress, DecodePaymentAddress and IsValidPaymentAddressString.

---
+++ src/sapling/key_io_sapling.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2016-2018 The Zcash developers
+// Copyright (c) 2020 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIVX_KEY_IO_SAPLING_H
+#define PIVX_KEY_IO_SAPLING_H
+
+#include "chainparams.h"
+
+namespace KeyIO {
+
+    std::string EncodePaymentAddress(const libzcash::PaymentAddress& zaddr);
+    libzcash::PaymentAddress DecodePaymentAddress(const std::string& str);
+    bool IsValidPaymentAddressString(const std::string& str);
+
+}
+
+#endif //PIVX_KEY_IO_SAPLING_H
