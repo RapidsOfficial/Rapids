303700e3e0795052fd36a2b47b8ee9f066747e5b
random-zebra <random.zebra@protonmail.com>
2020-03-25 01:41:43 +0100

    util: Refactor GetLogCategory.
    Changing parameter types from pointers to references and uint32_t to
    BCLog::LogFlags simplifies calling code.
    
    backports bitcoin@1eac317f25b905e97e311130ab19c3b0d257fc04

--- src/init.cpp
+++ src/init.cpp
@@ -941,11 +941,9 @@ bool AppInit2()
     if (!(GetBoolArg("-nodebug", false) ||
             find(categories.begin(), categories.end(), std::string("0")) != categories.end())) {
         for (const auto& cat : categories) {
-            uint32_t flag;
-            if (!GetLogCategory(&flag, &cat)) {
+            if (!g_logger->EnableCategory(cat)) {
                 UIWarning(strprintf(_("Unsupported logging category %s=%s."), "-debug", cat));
             }
-            g_logger->EnableCategory(static_cast<BCLog::LogFlags>(flag));
         }
     }
 
@@ -953,11 +951,9 @@ bool AppInit2()
     if (mapMultiArgs.count("-debugexclude") > 0) {
         const std::vector<std::string>& excludedCategories = mapMultiArgs.at("-debugexclude");
         for (const auto& cat : excludedCategories) {
-            uint32_t flag;
-            if (!GetLogCategory(&flag, &cat)) {
+            if (!g_logger->DisableCategory(cat)) {
                 UIWarning(strprintf(_("Unsupported logging category %s=%s."), "-debugexclude", cat));
             }
-            g_logger->DisableCategory(static_cast<BCLog::LogFlags>(flag));
         }
     }
 
