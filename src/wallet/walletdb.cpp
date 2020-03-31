1b193aedbf5e27efe899039a169f0cda671f79ca
furszy <matiasfurszyfer@protonmail.com>
2020-03-31 00:48:10 -0300

    [Wallet] load Sapling hd chain from disk.

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -724,6 +724,10 @@ bool ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue, CW
             CHDChain chain;
             ssValue >> chain;
             pwallet->GetScriptPubKeyMan()->SetHDChain(chain, true);
+        } else if (strType == "hdchain_sap") {
+            CHDChain chain;
+            ssValue >> chain;
+            pwallet->GetSaplingScriptPubKeyMan()->SetHDChain(chain, true);
         } else if (strType == "sapzkey") {
             libzcash::SaplingIncomingViewingKey ivk;
             ssKey >> ivk;
