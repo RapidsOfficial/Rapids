638fd8bec07b836f1f4ef108f52e01328aef238e
barrystyle <barrystyle@westnet.com.au>
2020-05-08 20:19:56 +0800

    Missed cheeky pivx uri: in dialog.

--- src/qt/openuridialog.cpp
+++ src/qt/openuridialog.cpp
@@ -17,7 +17,7 @@ OpenURIDialog::OpenURIDialog(QWidget* parent) : QDialog(parent, Qt::WindowSystem
 {
     ui->setupUi(this);
     this->setStyleSheet(parent->styleSheet());
-    ui->uriEdit->setPlaceholderText("pivx:");
+    ui->uriEdit->setPlaceholderText("rapids:");
 
     ui->labelSubtitle->setText("URI");
     setCssProperty(ui->labelSubtitle, "text-title2-dialog");
@@ -79,8 +79,8 @@ void OpenURIDialog::on_selectFileButton_clicked()
         }
 
         QString str = QString::fromStdString(std::string(r.constData(), r.length()));
-        if (!str.startsWith("pivx")) {
-            inform(tr("Invalid URI, not starting with \"pivx\" prefix"));
+        if (!str.startsWith("rapids")) {
+            inform(tr("Invalid URI, not starting with \"rapids\" prefix"));
         }
         ui->uriEdit->setText(str);
     }
