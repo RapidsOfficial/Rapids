a395914783d89bbeb9e28631a9b5ce54dde85e1d
random-zebra <random.zebra@protonmail.com>
2020-05-19 16:43:56 +0200

    Remove unused ReadVersion and WriteVersion
    CDataStream and CAutoFile had a ReadVersion and WriteVersion method
    that was never used. Remove them.
    
    backports bitcoin@50e8a9ccd78c8c20c3eac6618da7293078446b58

--- src/streams.h
+++ src/streams.h
@@ -208,8 +208,6 @@ public:
     int GetType() { return nType; }
     void SetVersion(int n) { nVersion = n; }
     int GetVersion() { return nVersion; }
-    void ReadVersion() { *this >> nVersion; }
-    void WriteVersion() { *this << nVersion; }
 
     CDataStream& read(char* pch, size_t nSize)
     {
@@ -365,8 +363,6 @@ public:
     int GetType() { return nType; }
     void SetVersion(int n) { nVersion = n; }
     int GetVersion() { return nVersion; }
-    void ReadVersion() { *this >> nVersion; }
-    void WriteVersion() { *this << nVersion; }
 
     CAutoFile& read(char* pch, size_t nSize)
     {
