13ad7792d34ac79f5a70f6bfbc65c44722fad6ca
random-zebra <random.zebra@protonmail.com>
2020-05-19 16:15:04 +0200

    add bip32 pubkey serialization
    backports bitcoin@90604f16af63ec066d6561337f476ccd8acec326
    
    PIVX: serialization not needed, only named constants

--- src/base58.h
+++ src/base58.h
@@ -179,8 +179,8 @@ public:
     CBitcoinExtKeyBase() {}
 };
 
-typedef CBitcoinExtKeyBase<CExtKey, 74, CChainParams::EXT_SECRET_KEY> CBitcoinExtKey;
-typedef CBitcoinExtKeyBase<CExtPubKey, 74, CChainParams::EXT_PUBLIC_KEY> CBitcoinExtPubKey;
+typedef CBitcoinExtKeyBase<CExtKey, BIP32_EXTKEY_SIZE, CChainParams::EXT_SECRET_KEY> CBitcoinExtKey;
+typedef CBitcoinExtKeyBase<CExtPubKey, BIP32_EXTKEY_SIZE, CChainParams::EXT_PUBLIC_KEY> CBitcoinExtPubKey;
 
 
 CTxDestination DestinationFor(const CKeyID& keyID, const CChainParams::Base58Type addrType);
