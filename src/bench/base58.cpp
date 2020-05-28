3f3eddec6f39cc84d6b8961e12d9e8ac67504931
random-zebra <random.zebra@protonmail.com>
2020-05-28 02:38:31 +0200

    [Bench] Use PIVX address in Base58Decode test

--- src/bench/base58.cpp
+++ src/bench/base58.cpp
@@ -45,7 +45,7 @@ static void Base58CheckEncode(benchmark::State& state)
 
 static void Base58Decode(benchmark::State& state)
 {
-    const char* addr = "17VZNX1SN5NtKa8UQFxwQbFeFc3iqRYhem";
+    const char* addr = "D6ytFXbsnEh7Y8NKgo84KA5yD8Uk4YnBdE";
     std::vector<unsigned char> vch;
     while (state.KeepRunning()) {
         DecodeBase58(addr, vch);
