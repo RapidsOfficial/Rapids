8dc509cbece7df2c2e0c9defae2831d72f3c6bfa
furszy <matiasfurszyfer@protonmail.com>
2020-06-04 17:55:36 -0300

    Trailing whitespaces cleanup over Sapling files
    Clean tabs instead of spaces

--- src/sapling/prf.cpp
+++ src/sapling/prf.cpp
@@ -14,20 +14,20 @@
 
 const unsigned char ZCASH_EXPANDSEED_PERSONALIZATION[crypto_generichash_blake2b_PERSONALBYTES] = {'Z','c','a','s','h','_','E','x','p','a','n','d','S','e','e','d'};
 
-// Sapling 
+// Sapling
 std::array<unsigned char, 64> PRF_expand(const uint256& sk, unsigned char t)
 {
-    std::array<unsigned char, 64> res;   
+    std::array<unsigned char, 64> res;
     unsigned char blob[33];
 
     memcpy(&blob[0], sk.begin(), 32);
     blob[32] = t;
-        
+
     crypto_generichash_blake2b_state state;
     crypto_generichash_blake2b_init_salt_personal(&state, nullptr, 0, 64, nullptr, ZCASH_EXPANDSEED_PERSONALIZATION);
     crypto_generichash_blake2b_update(&state, blob, 33);
     crypto_generichash_blake2b_final(&state, res.data(), 64);
-    
+
     return res;
 }
 
@@ -57,19 +57,19 @@ uint256 PRF_ovk(const uint256& sk)
 
 std::array<unsigned char, 11> default_diversifier(const uint256& sk)
 {
-    std::array<unsigned char, 11> res;   
+    std::array<unsigned char, 11> res;
     unsigned char blob[34];
 
     memcpy(&blob[0], sk.begin(), 32);
     blob[32] = 3;
-    
+
     blob[33] = 0;
     while (true) {
         crypto_generichash_blake2b_state state;
         crypto_generichash_blake2b_init_salt_personal(&state, nullptr, 0, 64, nullptr, ZCASH_EXPANDSEED_PERSONALIZATION);
         crypto_generichash_blake2b_update(&state, blob, 34);
         crypto_generichash_blake2b_final(&state, res.data(), 11);
-        
+
         if (librustzcash_check_diversifier(res.data())) {
             break;
         } else if (blob[33] == 255) {
@@ -77,7 +77,7 @@ std::array<unsigned char, 11> default_diversifier(const uint256& sk)
         }
         blob[33] += 1;
     }
-        
+
     return res;
 }
 
