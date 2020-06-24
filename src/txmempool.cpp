8ad82cac2bcf46880c607c650f4ade652938360a
furszy <matiasfurszyfer@protonmail.com>
2020-06-24 17:43:48 -0300

    Fix removal of time-locked transactions during reorg
    coming from btc@9b060e5cfb0d185b553b21ae19d390f81e83bd4d

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -10,6 +10,8 @@
 #include "main.h"
 #include "policy/fees.h"
 #include "streams.h"
+#include "timedata.h"
+#include "consensus/tx_verify.h"
 #include "util.h"
 #include "utilmoneystr.h"
 #include "utiltime.h"
@@ -458,23 +460,26 @@ void CTxMemPool::remove(const CTransaction& origTx, std::list<CTransaction>& rem
     }
 }
 
-void CTxMemPool::removeCoinbaseSpends(const CCoinsViewCache* pcoins, unsigned int nMemPoolHeight)
+void CTxMemPool::removeForReorg(const CCoinsViewCache* pcoins, unsigned int nMemPoolHeight)
 {
     // Remove transactions spending a coinbase which are now immature
     LOCK(cs);
     std::list<CTransaction> transactionsToRemove;
     for (indexed_transaction_set::const_iterator it = mapTx.begin(); it != mapTx.end(); it++) {
         const CTransaction& tx = it->GetTx();
-        for (const CTxIn& txin : tx.vin) {
-            indexed_transaction_set::const_iterator it2 = mapTx.find(txin.prevout.hash);
-            if (it2 != mapTx.end())
-                continue;
-
-            const CCoins *coins = pcoins->AccessCoins(txin.prevout.hash);
-            if (nCheckFrequency != 0) assert(coins);
-            if (!coins || ((coins->IsCoinBase() || coins->IsCoinStake()) && ((signed long)nMemPoolHeight) - coins->nHeight < (unsigned)Params().GetConsensus().nCoinbaseMaturity)) {
-                transactionsToRemove.push_back(tx);
-                break;
+        if (!IsFinalTx(tx, nMemPoolHeight, GetAdjustedTime())) {
+            transactionsToRemove.push_back(tx);
+        } else {
+            for (const CTxIn& txin : tx.vin) {
+                indexed_transaction_set::const_iterator it2 = mapTx.find(txin.prevout.hash);
+                if (it2 != mapTx.end())
+                    continue;
+                const CCoins *coins = pcoins->AccessCoins(txin.prevout.hash);
+                if (nCheckFrequency != 0) assert(coins);
+                if (!coins || ((coins->IsCoinBase() || coins->IsCoinStake()) && ((signed long)nMemPoolHeight) - coins->nHeight < (unsigned)Params().GetConsensus().nCoinbaseMaturity)) {
+                    transactionsToRemove.push_back(tx);
+                    break;
+                }
             }
         }
     }
