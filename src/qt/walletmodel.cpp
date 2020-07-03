67b4d74c41f10f9e168ad643249deac2bf84d528
Fuzzbawls <fuzzbawls@gmail.com>
2020-07-02 17:10:08 -0700

    [GUI] Return early in pollBalanceChanged
    When syncing or reindexing with a pre-existing wallet, there is no need
    to do any updating of the transaction table model data when the chain's
    tip time is prior to the wallet's creation time.

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -238,7 +238,14 @@ void WalletModel::pollBalanceChanged()
     if (!lockWallet)
         return;
 
-    int chainHeight = chainActive.Height();
+    // Don't continue processing if the chain tip time is less than the first
+    // key creation time as there is no need to iterate over the transaction
+    // table model in this case.
+    auto tip = chainActive.Tip();
+    if (tip->GetBlockTime() < getCreationTime())
+        return;
+
+    int chainHeight = tip->nHeight;
     if (fForceCheckBalanceChanged || chainHeight != cachedNumBlocks) {
         fForceCheckBalanceChanged = false;
 
