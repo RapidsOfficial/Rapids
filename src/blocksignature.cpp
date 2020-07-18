c9b41fef297b54aeab71e2e364c15819c6818565
furszy <matiasfurszyfer@protonmail.com>
2020-07-18 14:35:47 -0300

    Block signature better documentation.
    If the staking tx has a P2PK scriptsig input and a P2PKH scriptpubkey output, there is no way to obtain the public key from any of the scripts to verify the block signature.
    
    Added a good description and an early verification failed return (before it was failing on the pubkey verification, when the problem is actually the staking transaction entirely).

--- src/blocksignature.cpp
+++ src/blocksignature.cpp
@@ -73,11 +73,23 @@ bool CheckBlockSignature(const CBlock& block, const bool enableP2PKH)
         if (whichType == TX_PUBKEY) {
             valtype& vchPubKey = vSolutions[0];
             pubkey = CPubKey(vchPubKey);
-        }  else if (whichType == TX_PUBKEYHASH || whichType == TX_COLDSTAKE) {
+        } else if (whichType == TX_PUBKEYHASH) {
+            const CTxIn& txin = block.vtx[1].vin[0];
+            // Check if the scriptSig is for a p2pk or a p2pkh
+            if (txin.scriptSig.size() == 73) { // Sig size + DER signature size.
+                // If the input is for a p2pk and the output is a p2pkh.
+                // We don't have the pubkey to verify the block sig anywhere in this block.
+                // p2pk scriptsig only contains the signature and p2pkh scriptpubkey only contain the hash.
+                return false;
+            } else {
+                int start = 1 + (int) *txin.scriptSig.begin(); // skip sig
+                pubkey = CPubKey(txin.scriptSig.begin()+start+1, txin.scriptSig.end());
+            }
+        } else if (whichType == TX_COLDSTAKE) {
             // pick the public key from the P2CS input
             const CTxIn& txin = block.vtx[1].vin[0];
             int start = 1 + (int) *txin.scriptSig.begin(); // skip sig
-            if (whichType == TX_COLDSTAKE) start += 1 + (int) *(txin.scriptSig.begin()+start); // skip flag
+            start += 1 + (int) *(txin.scriptSig.begin()+start); // skip flag
             pubkey = CPubKey(txin.scriptSig.begin()+start+1, txin.scriptSig.end());
         }
     }
