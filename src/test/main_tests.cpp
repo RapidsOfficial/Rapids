28ab8844d921a71cca61fa0850452c7a14592132
furszy <matiasfurszyfer@protonmail.com>
2020-07-18 14:37:45 -0300

    Implemented a block signature verification unit test.
    Testing:
    
    1) P2PK block signature pre v5 enforcement.
    2) P2PK block signature post v5 enforcement.
    3) P2PKH block signature pre v5 enforcement.
    4) P2PKH block signature post v5 enforcement.

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -4,13 +4,112 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "primitives/transaction.h"
+#include "blocksignature.h"
 #include "main.h"
+#include "primitives/transaction.h"
+#include "script/sign.h"
 #include "test_pivx.h"
-
 #include <boost/test/unit_test.hpp>
 
 BOOST_FIXTURE_TEST_SUITE(main_tests, TestingSetup)
+
+enum BlockSignatureType{
+    P2PK,
+    P2PKH,
+    P2CS
+};
+
+CScript GetScriptForType(CPubKey pubKey, BlockSignatureType type)
+{
+    switch(type){
+        case P2PK:
+            return CScript() << pubKey << OP_CHECKSIG;
+        default:
+            return GetScriptForDestination(pubKey.GetID());
+    }
+}
+
+std::vector<unsigned char> CreateDummyScriptSigWithKey(CPubKey pubKey)
+{
+    std::vector<unsigned char> vchSig;
+    const CScript scriptCode;
+    DummySignatureCreator(nullptr).CreateSig(vchSig, pubKey.GetID(), scriptCode);
+    return vchSig;
+}
+
+CScript GetDummyScriptSigByType(CPubKey pubKey, bool isP2PK)
+{
+    CScript script = CScript() << CreateDummyScriptSigWithKey(pubKey);
+    if (!isP2PK)
+        script << ToByteVector(pubKey);
+    return script;
+}
+
+CBlock CreateDummyBlockWithSignature(CKey stakingKey, BlockSignatureType type, bool useInputP2PK)
+{
+    CMutableTransaction txCoinStake;
+    // Dummy input
+    CTxIn input(uint256(), 0);
+    // P2PKH input
+    input.scriptSig = GetDummyScriptSigByType(stakingKey.GetPubKey(), useInputP2PK);
+    // Add dummy input
+    txCoinStake.vin.emplace_back(input);
+    // Empty first output
+    txCoinStake.vout.emplace_back(CTxOut(0, CScript()));
+    // P2PK staking output
+    CScript scriptPubKey = GetScriptForType(stakingKey.GetPubKey(), type);
+    txCoinStake.vout.emplace_back(CTxOut(0, scriptPubKey));
+
+    // Now the block.
+    CBlock block;
+    block.vtx.emplace_back(CTransaction()); // dummy first tx
+    block.vtx.emplace_back(txCoinStake);
+    SignBlockWithKey(block, stakingKey);
+
+    return block;
+}
+
+bool TestBlockSignaturePreEnforcementV5(const CBlock& block)
+{
+    return CheckBlockSignature(block, false);
+}
+
+bool TestBlockSignaturePostEnforcementV5(const CBlock& block)
+{
+    return CheckBlockSignature(block, true);
+}
+
+BOOST_AUTO_TEST_CASE(block_signature_test)
+{
+    for (int i = 0; i < 20; ++i) {
+        CKey stakingKey;
+        stakingKey.MakeNewKey(true);
+        bool useInputP2PK = i % 2 == 0;
+
+        // Test P2PK block signature pre enforcement.
+        CBlock block = CreateDummyBlockWithSignature(stakingKey, BlockSignatureType::P2PK, useInputP2PK);
+        BOOST_CHECK(TestBlockSignaturePreEnforcementV5(block));
+
+        // Test P2PK block signature post enforcement
+        block = CreateDummyBlockWithSignature(stakingKey, BlockSignatureType::P2PK, useInputP2PK);
+        BOOST_CHECK(TestBlockSignaturePostEnforcementV5(block));
+
+        // Test P2PKH block signature pre enforcement ---> must fail.
+        block = CreateDummyBlockWithSignature(stakingKey, BlockSignatureType::P2PKH, useInputP2PK);
+        BOOST_CHECK(!TestBlockSignaturePreEnforcementV5(block));
+
+        // Test P2PKH block signature post enforcement
+        block = CreateDummyBlockWithSignature(stakingKey, BlockSignatureType::P2PKH, useInputP2PK);
+        if (useInputP2PK) {
+            // If it's using a P2PK scriptsig as input and a P2PKH output
+            // The block doesn't contain the public key to verify the sig anywhere.
+            // Must fail.
+            BOOST_CHECK(!TestBlockSignaturePostEnforcementV5(block));
+        } else {
+            BOOST_CHECK(TestBlockSignaturePostEnforcementV5(block));
+        }
+    }
+}
 
 CAmount nMoneySupplyPoWEnd = 43199500 * COIN;
 
