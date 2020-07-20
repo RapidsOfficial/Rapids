07a02f1eeb73ddf6e752e2508ef263b48153d635
furszy <matiasfurszyfer@protonmail.com>
2020-07-20 14:43:53 -0300

    Introduce wallet interface class and WalletBalances wrapper.

---
+++ src/interface/wallet.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2018-2020 The Bitcoin Core developers
+// Copyright (c) 2020 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#include "interface/wallet.h"
+#include "wallet.h"
+
+namespace interfaces {
+
+    WalletBalances Wallet::getBalances() {
+        WalletBalances result;
+        result.balance = m_wallet.GetBalance();
+        result.unconfirmed_balance = m_wallet.GetUnconfirmedBalance();
+        result.immature_balance = m_wallet.GetImmatureBalance();
+        result.have_watch_only = m_wallet.HaveWatchOnly();
+        if (result.have_watch_only) {
+            result.watch_only_balance = m_wallet.GetWatchOnlyBalance();
+            result.unconfirmed_watch_only_balance = m_wallet.GetUnconfirmedWatchOnlyBalance();
+            result.immature_watch_only_balance = m_wallet.GetImmatureWatchOnlyBalance();
+        }
+        result.zerocoin_balance = m_wallet.GetZerocoinBalance(false);
+        result.unconfirmed_zerocoin_balance = m_wallet.GetUnconfirmedZerocoinBalance();
+        result.immature_zerocoin_balance = m_wallet.GetImmatureZerocoinBalance();
+        result.delegate_balance = m_wallet.GetDelegatedBalance();
+        result.coldstaked_balance = m_wallet.GetColdStakingBalance();
+        return result;
+    }
+
+} // namespace interfaces
