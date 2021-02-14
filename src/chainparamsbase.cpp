03832255af512bd1ef2aa71c207a301e47b2ac40
volbil <volbilnexus@gmail.com>
2021-02-14 15:52:52 +0200

    Fixed RPC port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 51473;
+        nRPCPort = 28731;
     }
 };
 static CBaseMainParams mainParams;
