95eb51693ff8d388f86c286102394c60786f3fd8
volbil <volbilnexus@gmail.com>
2021-03-14 15:32:08 +0200

    Fix masternode collateral check

--- src/chainparams.h
+++ src/chainparams.h
@@ -87,9 +87,9 @@ public:
     /** Get masternode collateral */
     CAmount Collateral(int nHeight) const {
         if (nHeight >= consensus.height_supply_reduction)
-            return 10000000 * COIN;
+            return 10000 * COIN;
 
-        return 10000 * COIN;
+        return 10000000 * COIN;
     }
 
 
