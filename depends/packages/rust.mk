332d5b533c6bab1ab20c6b96c748acc60e9a75e4
volbil <volbilnexus@gmail.com>
2021-11-30 15:12:37 +0200

    Updated rust.md download path

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
 $(package)_version=1.42.0
-$(package)_download_path=https://depends.pivx.org
+$(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name_linux=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash_linux=5fed7d705e215fc129c4ace8060b5dc1a47e88228ce0249d48f30d769fcb6fe3
+$(package)_sha256_hash_linux=7d1e07ad9c8a33d8d039def7c0a131c5917aa3ea0af3d0cc399c6faf7b789052
 $(package)_file_name_darwin=rust-$($(package)_version)-x86_64-apple-darwin.tar.gz
 $(package)_sha256_hash_darwin=db1055c46e0d54b99da05e88c71fea21b3897e74a4f5ff9390e934f3f050c0a8
 $(package)_file_name_freebsd=rust-$($(package)_version)-x86_64-unknown-freebsd.tar.gz
@@ -12,18 +12,9 @@ $(package)_sha256_hash_freebsd=230bcf17e4383fba85d3c87fe25d17737459fe561a5f4668f
 # If a mapping is not present, we assume they are identical, unless $host_os is
 # "darwin", in which case we assume x86_64-apple-darwin.
 $(package)_rust_target_x86_64-w64-mingw32=x86_64-pc-windows-gnu
-$(package)_rust_target_i686-pc-linux-gnu=i686-unknown-linux-gnu
-$(package)_rust_target_riscv64-linux-gnu=riscv64gc-unknown-linux-gnu
-$(package)_rust_target_riscv64-unknown-linux-gnu=riscv64gc-unknown-linux-gnu
-$(package)_rust_target_x86_64-linux-gnu=x86_64-unknown-linux-gnu
-$(package)_rust_target_x86_64-pc-linux-gnu=x86_64-unknown-linux-gnu
 
 # Mapping from Rust targets to SHA-256 hashes
-$(package)_rust_std_sha256_hash_arm-unknown-linux-gnueabihf=f91d28115a46eb9af5bb73bb776269534e02add1125346b2dd7e29ecf34da892
-$(package)_rust_std_sha256_hash_aarch64-unknown-linux-gnu=782b2ab52d062ecc7077ddbfff1f21b553ac21845688a75aed45d70214a3c3e7
-$(package)_rust_std_sha256_hash_i686-unknown-linux-gnu=58191b8ccc78bbd288285a8e9b03c5e473ffbdf9d79c7262130202454f17c41f
-$(package)_rust_std_sha256_hash_x86_64-unknown-linux-gnu=016c9619bdbb8023876579ed97ade2f8b049696c2163f7518328b77b2c274f25
-$(package)_rust_std_sha256_hash_riscv64gc-unknown-linux-gnu=5496c43c340dfbef0a335498752e4efeff4534ada9a96754d9660be98ae8dc41
+$(package)_rust_std_sha256_hash_aarch64-unknown-linux-gnu=1343f51fc87049327233cee8941629c3d7dfdc425d359385f93665de3d46711b
 $(package)_rust_std_sha256_hash_x86_64-apple-darwin=1d61e9ed5d29e1bb4c18e13d551c6d856c73fb8b410053245dc6e0d3b3a0e92c
 $(package)_rust_std_sha256_hash_x86_64-pc-windows-gnu=8a8389f3860df6f42fbf8b76a62ddc7b9b6fe6d0fb526dcfc42faab1005bfb6d
 
