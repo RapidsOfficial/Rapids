e4b4fc561212ad66e3f98e3b9aaed31b60ae49cd
volbil <volbilnexus@gmail.com>
2022-05-24 15:01:51 +0300

    Reset RPDx testnet

--- src/tokencore/rules.cpp
+++ src/tokencore/rules.cpp
@@ -115,7 +115,7 @@ std::vector<TransactionCheckpoint> CMainConsensusParams::GetTransactions() const
 CMainConsensusParams::CMainConsensusParams()
 {
     // Exodus related:
-    GENESIS_BLOCK = 1813660;
+    GENESIS_BLOCK = 1827900;
     // Notice range for feature activations:
     MIN_ACTIVATION_BLOCKS = 10080;  // ~2 weeks
     MAX_ACTIVATION_BLOCKS = 60480; // ~12 weeks
